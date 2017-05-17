a8c357ef17e0759e57183e784e36f15685faabbf
Adam Dally <adamsdally@yahoo.com>
2017-05-17 10:20:19 -0400

    Basic player movement and text

---
+++ game/player.gd
@@ -0,0 +1,52 @@
+extends RigidBody2D
+
+# Character Properties
+var acceleration = 2000
+var top_move_speed = 200
+var top_jump_speed = 300
+
+# Movement Vars
+var directional_force = Vector2()
+const DIRECTION = {
+	ZERO = Vector2(0,0),
+	LEFT = Vector2(-1,0),
+	RIGHT = Vector2(1,0),
+	UP = Vector2(0,-1),
+	DOWN = Vector2(0,1)
+}
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _integrate_forces(state):
+	var final_force = Vector2()
+	
+	directional_force = DIRECTION.ZERO
+	
+	if Input.is_action_pressed("ui_left"):
+		directional_force += DIRECTION.LEFT
+	if Input.is_action_pressed("ui_right"):
+		directional_force += DIRECTION.RIGHT
+		
+	if Input.is_action_pressed("ui_up"):
+		#if !state.get_linear_velocity().y:
+		directional_force += DIRECTION.UP
+		
+	final_force = state.get_linear_velocity() + (directional_force * acceleration)
+	
+	# Prevent Exceeding Top Speed
+	if final_force.x < -top_move_speed:
+		final_force.x = -top_move_speed
+	if final_force.x > top_move_speed:
+		final_force.x = top_move_speed
+	
+	# Prevent Exceeding Top Jump Speed
+	if final_force.y < -top_jump_speed:
+		final_force.y = -top_jump_speed
+	#if final_force.y > top_jump_speed:
+	#	final_force.y = top_jump_speed
+	
+	state.set_linear_velocity(final_force)
+	
